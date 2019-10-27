5c77d0f7f9787cf78396ab228cf369251fa47421
xfhan <erazer_control@163.com>
2019-10-27 15:49:13 +0800

    发布v2.0.0

--- README.md
+++ README.md
@@ -6,8 +6,9 @@
 
 ### 公告
 ******
+- :heart:[10-27]发布[double11v2.0.0](https://github.com/ErazerControl/2019double11/releases),新增20+家店铺签到，感谢代码贡献者[@zijinggao](https://github.com/zijinggao)
 - :heart:[10-26]发布[double11v1.2.0](https://github.com/ErazerControl/2019double11/releases),新增在运行脚本前检测无障碍状态，未开启将跳转至设置页面，请在开启后返回至脚本运行界面！否则会导致脚本运行失败！！
-- :heart:[10-24]发布[double11v1.1.0beta版本](https://github.com/ErazerControl/2019double11/releases)，新增签到任务功能
+- :heart:[10-24]发布[double11v1.1.0](https://github.com/ErazerControl/2019double11/releases)，新增签到任务功能
 - :heart:[10-24]apk是我用网上auto.js打包工具打包的，安全性未知，不过我也在用，若出现任何问题与本人无关。apk下载地址[double11v1.0.0](https://github.com/ErazerControl/2019double11/releases)
 - :heart:[10-23]感谢star的小伙伴，已发布apk包 
 
@@ -17,9 +18,10 @@
 2. 打开淘宝喵铺主页 
 3. 运行double11
 4. 脚本会检测无障碍模式，若未开启会自动跳转至设置页面，请手动开启后返回至double11运行页面以免脚本运行失败
+5. 音量+键可以终止脚本
 ![淘宝喵铺主页](https://github.com/ErazerControl/2019double11/blob/master/images/taobao.jpg)
-![设置无障碍](https://github.com/ErazerControl/2019double11/blob/master/images/setting.jpg)
-![double11运行页面](https://github.com/ErazerControl/2019double11/blob/master/images/running.jpg)
+![设置无障碍](https://github.com/ErazerControl/2019double11/blob/master/images/settings.jpg)
+![double11运行页面](https://github.com/ErazerControl/2019double11/blob/master/images/run.jpg)
 
 ### 效果展示
 ******
@@ -29,7 +31,8 @@
 ******
 1. 按照[auto.js](https://github.com/hyb1996/Auto.js)部署开发环境
 2. 安卓手机打开淘宝喵铺主页
-3. 执行脚本
+3. 执行脚本  
+
 ![淘宝喵铺主页](https://github.com/ErazerControl/2019double11/blob/master/images/taobao.jpg)
 
 ### 功能简介
@@ -38,6 +41,9 @@
 * 完成签到任务
 ### 版本历史
 ******
+# v2.0.0
+###### 2019/10/27
+* `新增` 20+家店铺主页签到
 # v1.2.0
 ###### 2019/10/26
 * `修改` 智能检测无障碍模式是否开启
--- main.js
+++ main.js
@@ -1,59 +1,121 @@
 toast("检测是否开启无障碍模式")
 auto.waitFor()
-var appName = "手机淘宝";
-//广告版 depth(20) indexparent(0) text("OPPO官方旗舰店") text("GREE格力官方旗舰店 进店") 
-//广告版 text("Logo HR赫莲娜官方旗舰店 90.7万粉丝 进店")
-//非广告 depth(15) indexparent(0) desc("OPPO官方旗舰店天猫店")
- var shops = ["欧莱雅官方旗舰店","美的官方旗舰店","GREE格力官方旗舰店","苏泊尔官方旗舰店","小米官方旗舰店",
-        "荣耀官方旗舰店", "vivo官方旗舰店","OPPO官方旗舰店","李宁官方网店","olay官方旗舰店","YSL圣罗兰美妆官方旗舰店",
-        "蒙牛旗舰店","自然堂旗舰店","KIEHL'S科颜氏官方旗舰店","Lancome兰蔻官方旗舰店","Estee Lauder雅诗兰黛官方旗舰店天猫店",
-        "美特斯邦威官方网店","宝洁官方旗舰店","adidas官方旗舰店","奥克斯旗舰店","海尔官方旗舰店","HR赫莲娜官方旗舰店",
-        "GIORGIO ARMANI阿玛尼美妆官方旗舰店","波司登官方旗舰店"]
+var appName = "手机淘宝";
+var shops = ["欧莱雅官方旗舰店", "美的官方旗舰店", "GREE格力官方旗舰店", "苏泊尔官方旗舰店", "小米官方旗舰店",
+    "荣耀官方旗舰店", "vivo官方旗舰店", "OPPO官方旗舰店", "李宁官方网店", "olay官方旗舰店", "YSL圣罗兰美妆官方旗舰店",
+    "蒙牛旗舰店", "自然堂旗舰店", "KIEHL'S科颜氏官方旗舰店", "Lancome兰蔻官方旗舰店", "雅诗兰黛官方旗舰店天猫店",
+    "美特斯邦威官方网店", "宝洁官方旗舰店", "adidas官方旗舰店", "奥克斯旗舰店", "海尔官方旗舰店", "HR赫莲娜官方旗舰店",
+    "阿玛尼美妆官方旗舰店", "波司登官方旗舰店"]
 sleep(3000);
 launchApp(appName);
 sleep(3000);
-toast("即将找");
 //寻找领喵币按钮，存在则执行任务，否则退出脚本
-var lingmiaobi = indexInParent(5).depth(18).text("").findOnce();
-if (lingmiaobi) {
-    toast("找到按钮了")
-    lingmiaobi.click();
-    sleep(1000);
-    execTask();
+var lingmiaobi = indexInParent(5).depth(18).text("").findOnce();
+if (lingmiaobi) {
+    lingmiaobi.click();
+    sleep(1000);
+    execTask();
 }
-else {
-    toast("未检查到领喵币按钮")
+else {
+    toast("未检查到领喵币按钮")
 }
-function execTask() {
-    while (true) {
-        var target =  text("签到").findOnce() || text("去进店").findOnce() || text("去浏览").findOnce() ;
-        if (target == null) {
-            toast("任务完成");
-            break;
-        }
-        target.click();
-        sleep(3000);
-        if (target.text() === "签到") {
-            sleep(2000);
-            continue;
-        }
-        else {
-            //执行浏览广告类任务
-            viewWeb(20);
-        }
-        back();
-        sleep(2000);
-    }
+toast("即将执行店铺签到任务");
+back();
+sleep(1000);
+execShopCheckin(shops);
+toast("任务完成")
+function execTask() {
+    while (true) {
+        var target = text("签到").findOnce() || text("去进店").findOnce() || text("去浏览").findOnce();
+        if (target == null) {
+            break;
+        }
+        target.click();
+        sleep(3000);
+        if (target.text() === "签到") {
+            sleep(2000);
+            continue;
+        }
+        else {
+            //执行浏览广告类任务
+            viewWeb(20);
+        }
+        back();
+        sleep(2000);
+    }
 }
-function viewWeb(time) {
-    gesture(1000, [300, 600], [300, 300]);
-    var cnt = 1;
-    while (true) {
-        var finish = desc("任务完成").exists() || descStartsWith("已获得").exists();
-        if (finish || cnt > time) {
-            break;
-        }
-        sleep(1000);
-        cnt += 1;
-    }
+function viewWeb(time) {
+    gesture(1000, [300, 600], [300, 300]);
+    var cnt = 1;
+    while (true) {
+        var finish = desc("任务完成").exists() || descStartsWith("已获得").exists();
+        if (finish || cnt > time) {
+            break;
+        }
+        sleep(1000);
+        cnt += 1;
+    }
+}
+function execShopCheckin(shopName) {
+    var searchBar = desc("搜索").findOnce();
+    //点击首页的搜索按钮
+    if (searchBar) {
+        searchBar.click();
+        shopCheckin(shopName);
+    }
+    else {
+        toast("未找到搜索按钮")
+    }
+}
+function shopCheckin(shopName) {
+    for (var i = 0; i < shopName.length; ++i) {
+        //将搜索框内的内容替换成相应的店铺名并点击搜索
+        sleep(2000);
+        searchBar2 = depth(10).indexInParent(0).findOnce();
+        searchBar2.setText(shopName[i]);
+        sleep(500);
+        depth(9).indexInParent(2).text("搜索").findOne(5000).click();
+        //点击店铺
+        classNameStartsWith("android.support.v7.app").depth(11).indexInParent(2).clickable(true).findOne(5000).click();
+        //点击进店
+        // var jindian = depth(15).indexInParent(0).text("进店").findOne(3000).parent();
+        var jindian = depth(15).indexInParent(0).descContains(shopName[i]).clickable(true).findOne(3000) || depth(20).indexInParent(0).textContains(shopName[i]).clickable(true).findOne(3000);
+        if (jindian) {
+            jindian.click();
+            //双十一父亲 indexinparent(1) depth(10)
+            sleep(3000);
+            //点击双十一
+            var childofBtn = classNameStartsWith("android.widget.ImageView").depth(17).indexInParent(0).findOne(10000);
+            if (childofBtn) {
+                var btn = childofBtn.parent().parent();
+                // var btn = classNameStartsWith("android.widget.FrameLayout").depth(15).indexInParent(2).clickable(true).findOne(5000);
+
+                btn.click();
+                //点击签到领喵币
+                var qiandaoImg = desc("签到领喵币").findOne(5000);
+                if (qiandaoImg) {
+                    var qiandao = qiandaoImg.parent();
+                    qiandao.click();
+                    sleep(1000);
+                    log(shopName[i] + "签到领喵币");
+                }
+                else {
+                    log(shopName[i] + "没找到领喵币按钮")
+                }
+            }
+            else {
+                log(shopName[i] + "没找到双十一按钮")
+            }
+            //两次退回回到搜索框界面
+            while (desc("拍立淘").findOnce() == null) {
+                back();
+                sleep(1000);
+            }
+            back();
+        }
+        else {
+            log(shopName[i] + "没找到进店按钮");
+            back();
+        }
+    }
 }
